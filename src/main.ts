c50aef8e94b8c2e8c465cf93ed15b4456b2df3f8
Zhaoji Wang <hwoam@outlook.com>
2020-04-05 00:52:48 +0800

    feat: 引入helmet保护常见攻击，处理CORS跨域请求

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 
@@ -7,6 +8,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService)
   const port = configService.get('PORT')
   console.log(port)
+  // 保护一些著名的 Web 攻击
+  app.use(helmet())
+  // 让教务系统网站可以向程序发送 Ajax 请求
+  app.enableCors({
+    origin: ['http://zhjw.scu.edu.cn', 'http://202.115.47.141']
+  })
   await app.listen(port)
 }
 bootstrap()
