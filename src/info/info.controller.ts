c29f6540ad7bbe4c938083213e156edda62670db
Zhaoji Wang <hwoam@outlook.com>
2020-05-26 07:10:38 +0800

    fix: fixed typos

--- src/info/info.controller.ts
+++ src/info/info.controller.ts
@@ -4,6 +4,8 @@ import { Roles } from 'src/common/decorators/roles.decorator'
 import { UserRoleType } from 'src/user/user.interface'
 import { AuthGuard } from '@nestjs/passport'
 import { RolesGuard } from 'src/core/guards/roles.guard'
+import { BachelorDegree } from './info.entity'
+import { ScuUietpInfo } from './info.interface'
 
 @Controller('info')
 export class InfoController {
@@ -12,14 +14,20 @@ export class InfoController {
   @Get('bachelor_degree/:q')
   @Roles(UserRoleType.ADMIN, UserRoleType.NORMAL_USER)
   @UseGuards(AuthGuard(), RolesGuard)
-  getBachelorDegree(@Param('q') q: string) {
+  getBachelorDegree(@Param('q') q: string): Promise<BachelorDegree[]> {
     return this.infoService.findBachelorDegree(q)
   }
 
   @Get('scu_uietp/:q')
   @Roles(UserRoleType.ADMIN, UserRoleType.NORMAL_USER)
   @UseGuards(AuthGuard(), RolesGuard)
-  getScuUiep(@Param('q') q: string) {
+  getScuUiep(
+    @Param('q') q: string
+  ): Promise<{
+    query: string
+    number: number
+    list: ScuUietpInfo[]
+  }> {
     return this.infoService.findScuUietp(q)
   }
 }
